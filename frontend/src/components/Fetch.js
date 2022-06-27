2e81c987acf9866c0ac8875ea7e4a0fdb35caa8e
krishna-ncompass <sowndaryakrishnan@ncompass.inc>
2022-06-27 09:59:56 +0530

    Fixed the error on Frontend fetch page

--- frontend/src/components/Fetch.js
+++ frontend/src/components/Fetch.js
@@ -45,6 +45,8 @@ export default function Fetch() {
   return (
 
     <>
+    <br></br>
+    <div><h1>User's Details</h1></div>
     <div>
  <br>
  </br>
@@ -52,13 +54,13 @@ export default function Fetch() {
 <table id='customers'>
     <thead><tr className='table-row'>
       <th className='table-header'>Id</th>
-      <th className='table-header' >User's Name</th>
-      <th className='table-header' >Repository Name</th>
-      <th className='table-header' >URL of Repositories</th>
-      <th className='table-header' >User's Email</th>
+      <th className='table-header'>User's Name</th>
+      <th className='table-header'>Repository Name</th>
+      <th className='table-header'>URL of Repositories</th>
+      <th className='table-header'>User's Email</th>
       </tr></thead>
-<tbody> {first.map( (e) => {
-  return <tr>
+<tbody>{first.map( (e) => {
+  return <tr key={e.id} > 
     <td>{e.id}</td>
     <td>{e.username}</td>
     <td>{e.repository_name}</td>
@@ -66,12 +68,12 @@ export default function Fetch() {
     <td>{e.email}</td>
   </tr>
 
-}  ) } </tbody>
+}  ) }</tbody>
 
     </table>
     <br></br>
     <br></br>
-    <div className='button-container' ><button className='button-85' onClick={logout} >LOG OUT</button></div>
+    <div className='button-container'><button className='button-85' onClick={logout}>LOG OUT</button></div>
 </div>
     </>
 
